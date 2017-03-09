00c8a9dacd0f6cfd776869b1b6337a363a64db6d
Mike 'Anno' Phoohad <anno.tao@gmail.com>
2017-03-09 17:10:16 +0100

    Added deep embedded run

--- lio/LIO/Run.hs
+++ lio/LIO/Run.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE Trustworthy #-}
+{-# LANGUAGE GADTs #-}
 
 -- | This module contains functions to launch 'LIO' computations from
 -- within the 'IO' monad.  These functions are not useful from within
@@ -14,6 +15,9 @@ module LIO.Run (LIOState(..), runLIO, tryLIO, evalLIO, privInit) where
 import safe Control.Exception
 import safe qualified Control.Exception as IO
 import safe Data.IORef
+import safe Data.Typeable
+import safe Control.Monad
+import safe LIO.Exception (throwLIO)
 
 import safe LIO.Label
 import LIO.TCB
@@ -24,17 +28,48 @@ import LIO.TCB
 -- re-throw the exception, but the label state will always be valid.
 --
 -- See also 'evalLIO'.
-runLIO :: LIO l a -> LIOState l -> IO (a, LIOState l)
-runLIO (LIOTCB m) s0 = do
+runLIO :: Label l => LIO l a -> LIOState l -> IO (a, LIOState l)
+runLIO lio s0 = do
   sp <- newIORef s0
-  a <- m sp `IO.catch` \e -> return $ throw $ makeCatchable e
+  a <- runLIO' sp lio `IO.catch` \e -> return $ throw $ makeCatchable e
   s1 <- readIORef sp
   return (a, s1)
+  where
+    runLIO' :: Label l => IORef (LIOState l) -> LIO l a -> IO a
+    runLIO' ioRef lio =  case lio of
+      -- * Monadic actions
+      Return a -> return a
+      Bind ma k -> do
+        a <- runLIO' ioRef ma
+        runLIO' ioRef $ k a
+      Fail s -> fail s
+
+      -- * Internal state
+      GetLIOStateTCB -> readIORef ioRef
+      PutLIOStateTCB s -> writeIORef ioRef s
+      ModifyLIOStateTCB f -> do
+        s <- readIORef ioRef
+        writeIORef ioRef (f s)
+
+      -- * IO lift
+      IoTCB a -> a
+
+      -- * Exception handling
+      Catch lio' h ->
+        runLIO' ioRef lio' `IO.catch` \e -> runLIO' ioRef (safe e)
+        where
+          uncatchableType = typeOf (undefined :: UncatchableTCB)
+          safe e@(SomeException einner) = do
+            when (typeOf einner == uncatchableType) $ throwLIO e
+            LIOState l c <- getLIOStateTCB
+            unless (l `canFlowTo` c) $ throwLIO e
+            maybe (throwLIO e) h $ fromException e
+
 
 -- | A variant of 'runLIO' that returns results in 'Right' and
 -- exceptions in 'Left', much like the standard library 'try'
 -- function.
-tryLIO :: LIO l a -> LIOState l -> IO (Either SomeException a, LIOState l)
+tryLIO :: Label l => LIO l a -> LIOState l -> IO (Either SomeException a, LIOState l)
 tryLIO lio s0 = runLIO lio s0 >>= tryit
   where tryit (a, s) = do
           ea <- try (evaluate a)
@@ -53,7 +88,7 @@ tryLIO lio s0 = runLIO lio s0 >>= tryit
 --
 -- Unlike 'runLIO', this function throws an exception if the
 -- underlying 'LIO' action terminates with an exception.
-evalLIO :: LIO l a -> LIOState l -> IO a
+evalLIO :: Label l => LIO l a -> LIOState l -> IO a
 evalLIO lio s = do
   (a, _) <- runLIO lio s
   return $! a
